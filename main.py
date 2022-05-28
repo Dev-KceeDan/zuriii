c7940faaee0e3c2a3218488af31b2d080bd0a089
Dev Kcee <93603344+Dev-KceeDan@users.noreply.github.com>
2022-05-28 23:15:04 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
-# Read text from a file, and count the occurence of words in that text
-# Example:
-# count_words("The cake is done. It is a big cake!") 
-# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
-def read_file_content(filename):
-    # [assignment] Add your code here 
-    with open("./story.txt", "r") as openfile:
-        read_file = openfile.read()
-        # print(read_file)
-        # print("This file is true")
-    return read_file
-# readfile("./story.txt")
+from mimetypes import init
+from unicodedata import name
 
-def countwords():
-    text = read_file_content("./story.txt")
-    split_text = text.split()
-    # print(split_text)
+
+class Student:
+    name=str()
+    age = int()
+    tracks =list()
+    score = float()
     
-    count = {}
-    for i in split_text:
-        if i in count:
-            count[i] += 1
-        else: 
-            count[i] = 1
-    return count
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+      self.name = name
+      self.age = age
+      self.tracks = tracks
+      self.score = score
+      
+    def speak(self):
+        print("This is ", self.name, " and he is ", self.age, " years old. He is in the ", self.tracks, " track and he scored ", self.score, ". What an excellent student.")
 
-print(countwords())
+# new student details
+    print("New Student Details")
+    def change_name(self, new_name):
+        self.name = new_name
+        print("New name is ", self.name)
+        
+    def change_age(self, new_age):
+        self.age = int(new_age)
+        print("New age is ", self.age)
+        
+    def add_track(self, new_track):
+        self.tracks = new_track
+        print("New track name is ", self.tracks)
+        
+    def get_score(self):
+        print("Student's score is ", self.score)
+        
+#example instance 
+s = Student("Ray", 16,"Full time", "87%")
+print(s.change_name("Bobby"))
+print(s.change_age("25")) 
+print(s.add_track("UI/UX")) 
+print(s.get_score()) 
+
+
+
+def get_score(self, new_score):
+        print("Score: ", self.score)
